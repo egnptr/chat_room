15ae0ea47d833ac9ad761ac5b2a204a9be437b15
Eugene Ekaputra <eugeneekaputra@gmail.com>
2021-07-11 16:32:59 +0700

    Fix default profile pic

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -284,7 +284,7 @@ class ChatRoomListTile extends StatefulWidget {
 }
 
 class _ChatRoomListTileState extends State<ChatRoomListTile> {
-  String profilePicUrl = "", name = "", username = "";
+  String profilePicUrl = "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg", name = "", username = "";
 
   getThisUserInfo() async {
     username =
